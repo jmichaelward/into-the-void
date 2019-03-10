d592fcd72f2a7bea4937eb3a381f3c1d487cd487
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-10 12:14:21 -0500

    Change plugin name to Into the Void.

---
+++ into-the-void.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Into The Void
+ * Description: A personal status update feed for WordPress.
+ * Author: Jeremy Ward
+ * Author URI: https://jmichaelward.com
+ *
+ * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since  2019-03-10
+ */
