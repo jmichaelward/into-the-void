94ea72e0a93b0ce302b5b3faf3f7f2b57ea53094
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-10 12:17:37 -0500

    Scaffold bootstrap and main plugin class.

--- into-the-void.php
+++ into-the-void.php
@@ -8,3 +8,12 @@
  * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
  * @since  2019-03-10
  */
+
+$autoload = plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+if ( is_readable( $autoload ) ) {
+	require_once $autoload;
+}
+
+$plugin = new \JMichaelWard\IntoTheVoid\IntoTheVoid();
+$plugin->run();
---
+++ src/IntoTheVoid.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package JMichaelWard\IntoTheVoid
+ * @since   2019-03-10
+ */
+
+namespace JMichaelWard\IntoTheVoid;
+use WebDevStudios\OopsWP\Structure\ServiceRegistrar;
+
+/**
+ * Class IntoTheVoid
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package JMichaelWard\IntoTheVoid
+ * @since   2019-03-10
+ */
+class IntoTheVoid extends ServiceRegistrar {
+
+}
