53c0953ca376e7e1c1d633dffd6d627085eb48e5
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-16 09:42:04 -0500

    Add documentation for the content registration service.

--- src/Content/ContentRegistrar.php
+++ src/Content/ContentRegistrar.php
@@ -1,6 +1,8 @@
 <?php
 /**
+ * Content registration service.
  *
+ * This class handles registration of WordPress content objects.
  *
  * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
  * @package JMichaelWard\IntoTheVoid\Content
@@ -20,26 +22,45 @@ use WebDevStudios\OopsWP\Structure\Service;
  * @since   2019-03-10
  */
 class ContentRegistrar extends Service {
+	/**
+	 * Content types to register with WordPress.
+	 *
+	 * @var array
+	 * @since 2019-03-16
+	 */
 	protected $content_types = [
 		Shout::class,
 	];
 
+	/**
+	 * Register service hooks with WordPress.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-16
+	 */
 	public function register_hooks() {
 		add_action( 'init', [ $this, 'register_content_types' ] );
 	}
 
+	/**
+	 * Register content types with WordPress.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-16
+	 */
 	public function register_content_types() {
-		foreach( $this->content_types as $content_type_class ) {
+		foreach ( $this->content_types as $content_type_class ) {
 			$this->register_content_type( new $content_type_class() );
 		}
 	}
 
 	/**
-	 * @param ContentType $content_type
+	 * Register a ContentType object with WordPress.
+	 *
+	 * @param ContentType $content_type Instantiated ContentType.
 	 *
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
 	 * @since  2019-03-10
-	 * @return void
 	 */
 	private function register_content_type( ContentType $content_type ) {
 		$content_type->register();
